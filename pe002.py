6e7f38e705b8360decf92a2ff4ef991186c0c3bb
Joby Morey <nomadfarmer@gmail.com>
2018-09-26 18:56:17 -0400

    Fixed endless loop, better test of binet formula

--- pe002.py
+++ pe002.py
@@ -13,17 +13,21 @@ sequence up to 8.
 """
 
 def binet(n):
+    """ Binet's Formula finds the nth number in the fibonacci sequence """
     sq5 = 5 ** 0.5
     return round((1 / sq5) * ((((1 + sq5) / 2) ** n) - (((1 - sq5) / 2) ** n)))
 
 
+
 def main():
-    i = 3
+    ''' This code is to help me double check my implementation of the binet formula '''
+    i = 0
     while True:
+        i += 1
         fibi = binet(i)
         if fibi > 4 * 10 ** 16:
             break
-        print (i, fibi)
+        if fibi % 2 == 0: print (i, fibi)
 
         
 if __name__ == '__main__':
